d8f36782798a7e7da74362425c69408296166b72
Jhonni1000 <akintoyeopeyemi5@gmail.com>
2025-10-03 13:58:14 +0200

    fix: sqs visibility timeout

--- src/terraform/sqs.tf
+++ src/terraform/sqs.tf
@@ -4,7 +4,7 @@ resource "aws_sqs_queue" "request_queue" {
   max_message_size           = 2048
   message_retention_seconds  = 86400
   receive_wait_time_seconds  = 10
-  visibility_timeout_seconds = 60
+  visibility_timeout_seconds = 30
 
   redrive_allow_policy = jsonencode({
     deadLetterTargetArn = aws_sqs_queue.request_DLQ_queue.arn

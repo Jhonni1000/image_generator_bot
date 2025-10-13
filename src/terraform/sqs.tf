cfc7329571eda13917cd42d3207db1fec76ef053
Jhonni1000 <akintoyeopeyemi5@gmail.com>
2025-10-13 15:35:25 +0200

    fix: sqs permission for webhook lambda

--- src/terraform/sqs.tf
+++ src/terraform/sqs.tf
@@ -1,7 +1,13 @@
+# Dead Letter Queue
+resource "aws_sqs_queue" "request_DLQ_queue" {
+  name = "telegram_requests_queue_DLQ"
+}
+
+# Main Queue
 resource "aws_sqs_queue" "request_queue" {
   name                       = "telegram_requests_queue"
   delay_seconds              = 90
-  max_message_size           = 262124
+  max_message_size           = 262144
   message_retention_seconds  = 86400
   receive_wait_time_seconds  = 10
   visibility_timeout_seconds = 30
@@ -12,16 +18,37 @@ resource "aws_sqs_queue" "request_queue" {
   })
 }
 
-resource "aws_sqs_queue" "request_DLQ_queue" {
-  name = "telegram_requests_queue_DLQ"
+# Queue Policy (Allow Lambda to send messages)
+resource "aws_sqs_queue_policy" "request_queue_policy" {
+  queue_url = aws_sqs_queue.request_queue.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid = "AllowLambdaSendMessage"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Action = "sqs:SendMessage"
+        Resource = aws_sqs_queue.request_queue.arn
+        Condition = {
+          ArnLike = {
+            "aws:SourceArn" = aws_lambda_function.webhook_lambda.arn
+          }
+        }
+      }
+    ]
+  })
 }
 
+# Redrive Allow Policy
 resource "aws_sqs_queue_redrive_allow_policy" "request_queue_redrive_allow_policy" {
-  queue_url = aws_sqs_queue.request_queue.id
+  queue_url = aws_sqs_queue.request_DLQ_queue.id
 
   redrive_allow_policy = jsonencode({
     redrivePermission = "byQueue",
     sourceQueueArns   = [aws_sqs_queue.request_queue.arn]
   })
 }
-

9b39e4f7d5a3adde35749138ef2bb3fb3c5f2ba8
Jhonni1000 <akintoyeopeyemi5@gmail.com>
2025-10-13 15:58:40 +0200

    fix: remove sqs message delay time

--- src/terraform/sqs.tf
+++ src/terraform/sqs.tf
@@ -6,7 +6,6 @@ resource "aws_sqs_queue" "request_DLQ_queue" {
 # Main Queue
 resource "aws_sqs_queue" "request_queue" {
   name                       = "telegram_requests_queue"
-  delay_seconds              = 90
   max_message_size           = 262144
   message_retention_seconds  = 86400
   receive_wait_time_seconds  = 10

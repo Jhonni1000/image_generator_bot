005c3d80ca8012b87005d0522a406ec2d24d6a87
Jhonni1000 <akintoyeopeyemi5@gmail.com>
2025-10-31 22:52:16 +0100

    feat: implement state locking

--- src/terraform/backends.tf
+++ src/terraform/backends.tf
@@ -1,7 +1,20 @@
+resource "aws_dynamodb_table" "state-lock" {
+  name = "terraform-state-locks"
+  billing_mode = "PAY-PER-REQUEST"
+  hash_key = "LockId"
+
+  attribute {
+    name = "LockId"
+    type = "S"
+  }
+}
+
 terraform {
   backend "s3" {
     bucket = "telegram-image-generator-backend-19999"
     key    = "dev/terraform.tfstate"
     region = "us-east-1"
+    dynamodb_table = "terraform-state-locks"
+    encrypt = true
   }
 }
